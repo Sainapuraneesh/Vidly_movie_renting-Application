3ca4a01cbd9fcc55450e89bfab018237ad83c94f
Sainapuraneesh <Sainapuraneesh@gmail.com>
2025-11-22 14:53:40 +0530

    Added cloud mongodb connection string

--- startup/logging.js
+++ startup/logging.js
@@ -5,10 +5,12 @@ module.exports = function(){
     
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));//means it only writes errors to logfile.log,not on the terminal.
     winston.add(new winston.transports.Console({ colorize: true, prettyPrint: true }));//this helps to write error meassage on the terminal.
-    winston.add(new winston.transports.MongoDB({ 
+    if(NODE_ENV === 'test'){
+        winston.add(new winston.transports.MongoDB({ 
         db:'mongodb://localhost/vidly',
         level: 'info'
     }));//this creates new collection in database (called log) to store the error message.
+    }
     
     process.on('uncaughtException',(exe)=>{ //lec-150  It is event handler function which handles a event called "uncaughtException" 
         winston.info(exe.message);
